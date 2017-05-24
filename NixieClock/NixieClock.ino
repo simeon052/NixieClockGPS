05a1e84e4a8e0f2d5c2afd9f26bf2506b66e9532
Simeon052 <simeon_the_city_who_fought@outlook.com>
2017-05-24 21:59:27 +0900

    プロジェクト ファイルを追加します。

---
+++ NixieClock/NixieClock.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		NixieClock.ino
+ Created:	2017/05/24 21:59:11
+ Author:	Satoshi
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
